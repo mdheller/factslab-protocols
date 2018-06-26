29d2b3fa96a6d55bf24fdac83ff189adbc7c2974
venkatasg <gvenkata1994@gmail.com>
2018-06-26 10:47:32 -0400

    Final state of HTML and parse after running the protocol

--- genericity_protocol/pred_turk_parse.py
+++ genericity_protocol/pred_turk_parse.py
@@ -1,10 +1,13 @@
 from predpatt import load_conllu
 from predpatt import PredPatt
+from predpatt import PredPattOpts
+from predpatt.filters import isNotCopula
 import json
 from os.path import expanduser
 import re
 import random
-from pilot_ids import pilot_ids
+from collections import Counter
+import pdb
 
 
 def htmlify(s):
@@ -41,68 +44,89 @@ def replace_string(s):
     return s
 
 
-copula_verbs = ['be', 'am', 'is', 'are', 'was', 'were', 'being', 'been', 'r',
-                '&#39;m', '&#39;s', 's']
-
 id = 1
-files = ['/UD_English-r1.2/en-ud-train.conllu', '/UD_English-r1.2/en-ud-dev.conllu']
+files = ['/UD_English-r1.2/en-ud-dev.conllu',
+         '/UD_English-r1.2/en-ud-test.conllu']
 home = expanduser("~/Downloads/")
-parsed = []
+parsed = {'train': [], 'devte': []}
 out_data = []
+
+# Resolve relative clause
+options = PredPattOpts(resolve_relcl=True, borrow_arg_for_relcl=True, resolve_conj=False)
+
+path = home + '/UD_English-r1.2/en-ud-train.conllu'
+with open(path, 'r') as infile:
+    data = infile.read()
+    parsed['train'] += [('en-ud-train.conllu' + " " + sent_id, PredPatt(ud_parse, opts=options)) for sent_id, ud_parse in load_conllu(data)]
 
 for file in files:
     path = home + file
     with open(path, 'r') as infile:
         data = infile.read()
-        parsed += [(file[17:] + " " + sent_id, PredPatt(ud_parse)) for
-            sent_id, ud_parse in load_conllu(data)]
+        parsed['devte'] += [(file[17:] + " " + sent_id, PredPatt(ud_parse, opts=options)) for sent_id, ud_parse in load_conllu(data)]
+# random.shuffle(parsed['train'])
+c = {'train': 0, 'devte': 0}
+copp = {'train': 0, 'devte': 0}
+auxverb = {'train': 0, 'devte': 0}
+ign = {'train': 0, 'devte': 0}
 
-random.shuffle(parsed)
-pilot_ids = pilot_ids()             # If you want to do pilot study
-pilot_parsed = []
+for write_file in ['pred_train_data.csv', 'pred_devte_data.csv']:
+    dat = write_file[5:10]
+    with open(write_file, 'w+') as outfile:
+        id = 1
+        outfile.write("var_arrays\n")
+        for sent_id, parse_sen in parsed[dat]:
+            raw_sentence = " ".join([token.text for token in parse_sen.tokens])
+            html_sentence = htmlify(raw_sentence)
+            sent_preds = []
+            for predicate in parse_sen.instances:
+                sent_check = [pr.position for pr in sent_preds]
+                if predicate.position not in sent_check:
+                    sent_preds.append(predicate)
 
-for sent_id in pilot_ids:
-    for x, y in parsed:
-        if x == sent_id:
-            ud_parse = y
-            break
-    pilot_parsed.append((sent_id, ud_parse))
+            for predicate in sent_preds:
+                if predicate.root.tag not in ["ADJ", "NOUN", "NUM", "DET", "PROPN", "PRON", "VERB", "AUX"]:
+                    ign[dat] += 1
+                    continue
+                if predicate.root.tag not in ["VERB", "AUX"]:
+                    gov_rels = [tok.gov_rel for tok in predicate.tokens]
+                    all_pred = [t for t in predicate.tokens]
+                    if 'cop' in gov_rels:
+                        # Highlight from cop to end of pred.text
+                        copp[dat] += 1
+                        cop_pos = gov_rels.index('cop')
+                        pred = [x.text for x in all_pred[cop_pos:]]
+                        pred_token = [x.position for x in all_pred[cop_pos:]]
+                    else:
+                        ign[dat] += 1
+                        continue
+                else:
+                    # Just choose the verb or aux root as pred
+                    auxverb[dat] += 1
+                    pred = [predicate.root.text]
+                    pred_token = [predicate.root.position]
+                c[dat] += 1
+                token_dict = {}
+                pred_sentence = html_sentence.split().copy()
+                acc = 0
+                for ins in pred_token:
+                    pred_sentence.insert(ins + acc, ' <span class=\"predicate\">')
+                    pred_sentence.insert(ins + acc + 2, '</span> ')
+                    acc += 2
+                token_dict['raw_sentence'] = html_sentence
+                token_dict['id'] = id
+                token_dict['predicate'] = htmlify(" ".join(pred))
+                pred_token = ",".join(map(str, pred_token))
+                token_dict['pred_token'] = htmlify(pred_token)
+                token_dict['pred_sentence'] = " ".join(pred_sentence)
+                token_dict['sent_id'] = sent_id
 
-# Create dictionary for each predicate token and write as json dump
-# Create a new line after every 10 tokens - each HIT has 10 questions
-with open('pilot_pred_dataset.csv', 'w+') as outfile:
-    outfile.write("var_arrays\n")
-    for sent_id, parse_sen in pilot_parsed:
-        raw_sentence = " ".join([token.text for token in parse_sen.tokens])
-        html_sentence = htmlify(raw_sentence)
-        for predicate in parse_sen.instances:
-            # print(raw_sentence, parse_sen.instances)
-            if predicate.root.tag != "VERB":
-                try:
-                    pred = list(set(html_sentence.lower().split()) & set(copula_verbs))[0]
-                    pred_token = html_sentence.split().index(pred)
-                except IndexError:
-                    pred = predicate.root.text
-                    pred_token = predicate.root.position
-                # print("PRED1:", pred)
-            else:
-                pred = predicate.root.text
-                pred_token = predicate.root.position
-                # print("PRED2:", pred, predicate.root.tag)
-            token_dict = {}
-            pred_sentence = html_sentence.split().copy()
-            pred_sentence.insert(pred_token, ' <span class=\"predicate\">')
-            pred_sentence.insert(pred_token + 2, '</span> ')
-            token_dict['raw_sentence'] = html_sentence
-            token_dict['id'] = id
-            token_dict['pred'] = pred
-            token_dict['pred_token'] = str(pred_token)
-            token_dict['pred_sentence'] = " ".join(pred_sentence)
-            token_dict['sent_id'] = sent_id
-            out_data.append(json.dumps(token_dict))
-            id += 1
-            if id == 11:
-                id = 1
-                outfile.write("\"" + replace_string(str(out_data)) + "\"\n")
-                out_data = []
-    # outfile.write("\"" + replace_string(str(out_data)) + "\"\n")
+                out_data.append(json.dumps(token_dict))
+                id += 1
+                if id == 11:
+                    id = 1
+                    outfile.write("\"" + replace_string(str(out_data)) + "\"\n")
+                    out_data = []
+        outfile.write("\"" + replace_string(str(out_data)) + "\"\n")
+        out_data = []
+print("INCL", c, "COP", copp, "VERB", auxverb, "IGN", ign)
