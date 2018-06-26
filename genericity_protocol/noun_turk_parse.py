9f9c91bcb30a26e8c4578e4850f585aed9218b2b
venkatasg <gvenkata1994@gmail.com>
2018-06-26 11:03:58 -0400

    Updated versions of html and parse after annotations have been completed

--- genericity_protocol/noun_turk_parse.py
+++ genericity_protocol/noun_turk_parse.py
@@ -1,11 +1,12 @@
 from predpatt import load_conllu
 from predpatt import PredPatt
+from predpatt import PredPattOpts
 import json
 from os.path import expanduser
 import re
 import random
 from pilot_ids import pilot_ids
-
+import pdb
 
 def html_ify(s):
     '''
@@ -41,70 +42,79 @@ def replace_string(s):
     return s
 
 
-copula_verbs = ['be', 'am', 'is', 'are', 'was', 'were', 'being', 'been']
-
-id = 1
-files = ['/UD_English-r1.2/en-ud-train.conllu', '/UD_English-r1.2/en-ud-dev.conllu']
+files = ['/UD_English-r1.2/en-ud-dev.conllu',
+         '/UD_English-r1.2/en-ud-test.conllu']
 home = expanduser("~/Downloads/")
-parsed = []
+parsed = {'train': [], 'devte': []}
 out_data = []
+
+options = PredPattOpts(resolve_relcl=True, borrow_arg_for_relcl=True, resolve_conj=False)  # Resolve relative clause
+
+path = home + '/UD_English-r1.2/en-ud-train.conllu'
+with open(path, 'r') as infile:
+    data = infile.read()
+    parsed['train'] += [('en-ud-train.conllu' + " " + sent_id, PredPatt(ud_parse, opts=options)) for sent_id, ud_parse in load_conllu(data)]
 
 for file in files:
     path = home + file
     with open(path, 'r') as infile:
         data = infile.read()
-        parsed += [(file[17:] + " " + sent_id, PredPatt(ud_parse)) for
+        parsed['devte'] += [(file[17:] + " " + sent_id, PredPatt(ud_parse, opts=options)) for
             sent_id, ud_parse in load_conllu(data)]
 
-random.shuffle(parsed)
-pilot_ids = pilot_ids()             # If you want to do pilot study
-pilot_parsed = []
+c = {'train': 0, 'devte': 0}
+ign = {'train': 0, 'devte': 0}
+prons_incl = ["you", "they", "yourself", "themselves", "them", "themself",
+              "theirself", "theirselves"]
 
-for sent_id in pilot_ids:
-    for x, y in parsed:
-        if x == sent_id:
-            ud_parse = y
-            break
-    pilot_parsed.append((sent_id, ud_parse))
+for write_file in ['noun_train_data.csv', 'noun_devte_data.csv']:
+    dat = write_file[5:10]
+    with open(write_file, 'w+') as outfile:
+        id = 1
+        outfile.write("var_arrays\n")
+        for sent_id, parse_sen in parsed[dat]:
+            raw_sentence = " ".join([token.text for token in parse_sen.tokens])
+            html_sentence = html_ify(raw_sentence)
+            sent_args = []
+            for predicate in parse_sen.instances:
+                if predicate.root.tag in ["NOUN"]:
+                    # Include noun predicates in arguments
+                    all_args = predicate.arguments + [predicate]
+                else:
+                    all_args = predicate.arguments
+                sent_check = [x.position for x in sent_args]
+                for each_arg in all_args:
+                    if each_arg.position not in sent_check:
+                        sent_args.append(each_arg)
 
-with open('noun_dataset.csv', 'w+') as outfile:
-    outfile.write("var_arrays\n")
-    # Replace parsed with pilot_parsed for pilot data
-    for sent_id, parse_sen in parsed:
-        raw_sentence = " ".join([token.text for token in parse_sen.tokens])
-        raw_sentence = html_ify(raw_sentence)
-        # if len(parse_sen.instances) == 1:
-        for predicate in parse_sen.instances:
-            # if predicate.root.tag == "NOUN":
-                # pred = list(set(raw_sentence.split()) & set(copula_verbs))[0]
-                # pred_pos = raw_sentence.split().index(pred)
-            # else:
-                # pred = predicate.root.text
-                # pred_pos = predicate.root.position
-            # for argument in (predicate.arguments + [predicate]):
-            # if len(predicate.arguments) == 3:
-            for argument in predicate.arguments:
-                if argument.root.gov_rel in ['nsubj', 'dobj', 'obj', 'iobj', 'obl', 'vocative', 'expl', 'nmod', 'appos', 'nummod']:
+            for argument in sent_args:
+                if argument.root.tag in ["DET", "NUM", "NOUN", "PROPN", "PRON"]:
+                    if argument.root.tag == "PRON":
+                        if argument.root.text.lower() not in prons_incl:
+                            ign[dat] += 1
+                            continue
+                    c[dat] += 1
                     noun = argument.root.text
                     token_dict = {}
                     noun = html_ify(noun)
                     noun_token = argument.root.position
-                    noun_sentence = raw_sentence.split().copy()
+                    noun_sentence = html_sentence.split().copy()
                     noun_sentence.insert(noun_token, ' <span class=\"noun\">')
                     noun_sentence.insert(noun_token + 2, '</span> ')
-                    token_dict['raw_sentence'] = raw_sentence
+                    token_dict['raw_sentence'] = html_sentence
                     token_dict['id'] = id
                     token_dict['noun'] = noun
-                    token_dict['noun_token'] = str(noun_token)
+                    token_dict['noun_token'] = html_ify(str(noun_token))
                     token_dict['noun_sentence'] = " ".join(noun_sentence)
-                    token_dict['sent_id'] = sent_id
-                    token_dict['predicate'] = predicate.root.text
-                    token_dict['pred_token'] = predicate.root.position
+                    token_dict['sent_id'] = html_ify(sent_id)
+                    token_dict['predicate'] = html_ify(predicate.root.text)
+                    token_dict['pred_token'] = html_ify(str(predicate.root.position))
                     out_data.append(json.dumps(token_dict))
-                    # print(sent_id, " ".join(noun_sentence))
-
                     id += 1
                     if id == 11:
                         id = 1
                         outfile.write("\"" + replace_string(str(out_data)) + "\"\n")
                         out_data = []
+        outfile.write("\"" + replace_string(str(out_data)) + "\"\n")
+        out_data = []
+print("INCL", c, "IGN", ign)
